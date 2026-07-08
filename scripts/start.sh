8150a5ea3d06c39c86ac5dbf539925cfc0c069d0
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2026-07-07 21:41:02 -0400

    Allow changing the env file without needing a rebuild

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-GH_OWNER=$GH_OWNER
-GH_REPOSITORY=$GH_REPOSITORY
-GH_TOKEN=$GH_TOKEN
+source /home/docker/.env
 
 REG_TOKENS=()
 
@@ -23,6 +21,8 @@ trap 'cleanup; exit 143' TERM
 
 for (( i=0; i<${#REPOS[@]}; ++i)); do
     REPOSITORY="${REPOS[$i]}"
+    echo Setting up ${REPOSITORY}...
+
     RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
     RUNNER_NAME="dockerNode-${RUNNER_SUFFIX}"
     REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
